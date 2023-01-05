9852ab7da041a603edb22103c60c13e9b4795e68
greentea <michaelhaneman.frc@gmail.com>
2023-01-05 04:06:17 -0800

    some physics cleanup

--- scripts/game/player.gd
+++ scripts/game/player.gd
@@ -36,7 +36,7 @@ func _process(_delta):
 			spawn_player()
 		else:
 			gamebus.stage_number = 1
-			gamebus.base_scale = 1.5
+			gamebus.base_scale = gamebus.INIT_PLAT_SIZE
 			get_tree().change_scene_to_file("res://scenes/menu_scene/menu.tscn")
 	
 	
@@ -47,27 +47,39 @@ func _process(_delta):
 		
 	camera.rotation.y = lerp_angle(camera.rotation.y, theta, ROTATE_SPEED * speed)
 	body.rotation.y = lerp_angle(body.rotation.y, theta, 0.5)
+	
 
+
+var collision_vel_gain:float = 0.0 # how fast were we falling before hitting the ground
+const COLLISION_ENERGY_LOSS = 0.08 # how much velocity is retained from collision w/ ground
+var gained_speed:float = 0.0 # the amount of speed gained from falling -> applied to player
+const FRICTION:float = 0.05
+const TERM_GAIN:float = 10.0 # limit on how much speed we can gain from falling
+const MIN_GAIN:float = 0.2 # implement slowing down a little when going uphill
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# jump uses a single firework
+	# controls
 	if Input.is_action_just_pressed("jump") && gamebus.collected_fireworks > 0:
 		gamebus.collected_fireworks -= 1
 		velocity.y = JUMP_VELOCITY
-	
 	if Input.is_action_just_pressed("down") and !is_on_floor():
 		velocity.y = FAST_FALL
-	
-	
 	if Input.is_action_just_pressed("left"):
 		direction = direction.rotated(-PI/2)
 	if Input.is_action_just_pressed("right"):
 		direction = direction.rotated(PI/2)
 		
-	velocity.x = direction.x * speed
-	velocity.z = direction.y * speed
+	#physics
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		# probably change to non-linear func
+		collision_vel_gain = -velocity.y * COLLISION_ENERGY_LOSS
+	else:
+		gained_speed += collision_vel_gain
+		gained_speed = clampf(gained_speed - FRICTION, 0, TERM_GAIN)
+		collision_vel_gain = 0.0
+		
+	velocity.x = direction.x * (speed + gained_speed)
+	velocity.z = direction.y * (speed + gained_speed)
 	
 	move_and_slide()
 	
@@ -77,10 +89,8 @@ func _on_portal_entered():
 	self.global_position = Vector3(0, 3, 3)
 	direction = INIT_DIR
 	theta = 0
+	gained_speed = 0
 	speed = clampf(speed + ACCEL, 0, MAX_SPEED)
-	print(speed)
-	
-	print("portal entered -> player")
 	
 	gamebus.stage_number += 1
 	gamebus.base_scale = clampf(
